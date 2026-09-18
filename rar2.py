75d96ef3d37b8bcfc3c7737baeaca9299fab5b71
daria gornaia <gornaia.ds@phystech.edu>
2026-09-18 16:19:39 +0300

    turtle program

---
+++ rar2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import turtle
+turtle.shape('turtle')
+for turtle in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+
+    
---
+++ rar3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import turtle
+turtle.shape('turtle')
+for turtle in range(360):
+    turtle.forward(1)
+    turtle.left(1)
+
+
+
